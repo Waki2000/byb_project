b6444a2af0caa1ca04a6c8a8f465eb402fd925c3
“Gladiator” <panjerian@gmail.com>
2024-03-12 01:02:23 +0000

    first commit

---
+++ helloWorld.py
@@ -0,0 +1,3 @@
+#user_input =  input(" input something please!").lower() # asking user input and changing it to lower case
+#print("Git is awesome! " + user_input) # displaying user input and more input
+print("Git is Awesome!")
